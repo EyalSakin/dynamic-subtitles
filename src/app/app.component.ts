40cbbec9c9485b575ae946c19cb00abf4000cd9d
Eyal Sakin <eyalsakin@viz2s-MacBook-Pro.local>
2023-06-11 02:56:44 +0300

    add functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+const DEFAULT_VTT_CONTENT: string =
+`WEBVTT
+
+00:00:01.000 --> 00:02.500
+- Uncle, you better come look at this.
+
+00:00:02.600 --> 00:03.400
+- What?
+
+00:00:03.500 --> 00:06.000
+- There's a man standing in the street.`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dynamic-subtitles';
+export class AppComponent implements OnInit {
+  public text: string = DEFAULT_VTT_CONTENT;
+  public subtitlesSrc: string = '';
+
+  ngOnInit() {
+    this.generateSubtitles();
+  }
+
+  public generateSubtitles() {
+    const blob: Blob = new Blob([this.text], { type: 'text/vtt' });
+    this.subtitlesSrc = URL.createObjectURL(blob);
+  }
 }
