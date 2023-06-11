5fb52d46b978c7939e346564c2d941180bbaf3f5
Eyal Sakin <eyalsakin@viz2s-MacBook-Pro.local>
2023-06-11 03:05:27 +0300

    move the default vtt text to a separated file

---
+++ src/app/default-vtt-content.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_VTT_CONTENT: string =
+`WEBVTT
+
+00:00:01.000 --> 00:02.500
+- Uncle, you better come look at this.
+
+00:00:02.600 --> 00:03.400
+- What?
+
+00:00:03.500 --> 00:06.000
+- There's a man standing in the street.`;
